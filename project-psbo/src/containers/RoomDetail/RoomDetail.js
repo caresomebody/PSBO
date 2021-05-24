acec683a8b6cb487b4687ebb644851528ad2322d
Rayhanendra <mrayhann@gmail.com>
2021-05-24 13:43:40 +0700

    Refactor Folder

---
+++ project-psbo/src/containers/RoomDetail/RoomDetail.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function RoomDetail() {
+  return <div></div>;
+}
+
+export default RoomDetail;
--- project-psbo/src/navigation/RouterConfig.js
+++ project-psbo/src/navigation/RouterConfig.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Route, Switch } from "react-router";
 import Login from "containers";
-import Home from "containers/Beranda/Beranda";
+import Home from "containers/Home/Home";
+import RoomDetail from "containers/RoomDetail/RoomDetail";
 
 function RouterConfig() {
   return (
     <Switch>
       <Route exact path="/" component={Login} />
       <Route exact path="/beranda" component={Home} />
+      <Route exact path="/detail-ruangan" component={RoomDetail} />
     </Switch>
   );
 }
